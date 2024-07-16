87b5b95bfd738aa3fd99e920eb7dad8a7ee28e9d
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-07-16 10:50:41 +0530

    loops is completed

---
+++ basics.js
@@ -0,0 +1,286 @@
+//  let var const
+
+
+// const  a=5 ;
+// let b=6 
+// console.log(a+b)
+//  console.log("hello")
+
+//  string and variable  
+
+
+ let Name="Harry"
+ let Name1="jdgnjdn"
+
+//  console.log(Name1)
+//  console.log("jjjjjjjjjjbjbjbjbjbj")
+let MyName ='My name is Shivam' 
+//  console.log("MiiyName")
+
+
+
+//  let a = 10
+//   a = "hello"
+//  a=true
+//   console.log(a)
+//  console.log(4==5)
+
+// let number="544"
+// if(number<5){
+//     console.log("no is less than 5")
+// }else if(number<10){
+//     console.log("no is less than 10")
+// }else{
+//     console.log("no is greater than 10")
+// }
+
+// even odd numbers
+
+// let num=1300
+//     num=50
+    
+// console.log(num)
+// console.log(50==='50')
+
+
+// if(num%2==0){
+//     console.log("even")
+// }else{
+//     console.log("odd")
+// }
+
+
+// operators 
+// let v = 88
+// let n="kjsk"
+// let m= 10+20+n +v 
+//  console.log( typeof m)
+
+// console.log(10-20)
+
+// AND and OR 
+// let c= true
+
+// if(c<100 && c>0){
+//     console.log("Its a valid number")
+// }else{
+//     console.log("Its a invalid number")
+// }
+
+// if(typeof c ==='number' || typeof c === 'string'){
+//     console.log("its a valid input")
+// }else{
+//     console.log("its a invalid input")
+// }
+
+
+// let User = 'Login'
+
+// if(User ==='Admin' || User ==='Login'){
+//     console.log("Welcome to DashBooard")
+// }else{
+//     console.log("Please Login to access the DashBoard")
+// }
+
+// you have to find the greatest between three numbers 
+
+
+
+let a= 20
+let b= 20
+
+// if(a>b){
+//     console.log("a is greater than b")
+// }else if(b>a){
+//     console.log("b is greater than a")
+// }
+// else{
+//     console.log("both are equal")
+// }
+
+// if(a>b){
+//     console.log("a is greater than b")
+// }else if(b==a){
+//     console.log("both are equal")
+// }
+// else{
+//     console.log("b is greater than a")
+// }
+
+// let x= 500
+// let y= 4000
+// let z= 300
+
+
+// if(x>y && x>z){
+//     console.log("x is greater than y and z")
+//     }else if( y>z){
+//         console.log("y is greater than x and z")
+//         }else{
+//             console.log("z is greater than x and y")
+//         }
+
+// if(x>y){
+//     if(x>z){
+//         console.log("x is greater than y and z")
+//         }else{
+//             console.log("z is greater than x and y")
+//             }
+// }else{
+//       if(y>z){
+//         console.log("y is greater than x and z")
+//         }else{
+//             console.log("z is greater than x and y")
+//             }
+
+// }
+
+
+// create a variable name day , assign value from 0 - 7 ,
+// output will be ""sunday"  if day is equal to 0 and saturday for value 7 of day
+
+// let day= 20
+
+// if(day==0){
+//     console.log("sunday")
+//     }else if(day==1){
+//         console.log("monday")
+//         }else if(day==2){
+//             console.log("tuesday")
+//             }else if(day==3){
+//                 console.log("wednesday")
+//                 }else if(day==4){
+//                     console.log("thursday")
+//                     }else if(day==5){
+//                         console.log("friday")
+//                         }else if(day==6){
+//                             console.log("saturday")
+//                             }else{
+//                                 console.log("invalid day")
+//                                 }
+
+// switch case
+
+// let day= 3
+
+// switch(day){
+//     case 0:
+//         console.log("sunday")
+//         break;
+//         case 1:
+//             console.log("monday")
+//             break;
+//             case 2:
+//                 console.log("tuesday")
+//                 break;
+//                 case 3:
+//                     console.log("wednesday")
+//                     break;
+//                     case 4:
+//                         console.log("thursday")
+//                         break;
+//                         case 5:
+//                             console.log("friday")
+//                             break;
+//                             case 6:
+//                                 console.log("saturday")
+//                                 break;
+//                                 default:
+//                                     console.log("invalid day")
+//                                     break;
+//                                     }
+
+//                                 1    //switch case with string
+                            //  2   //    greatest amoung four numbers
+                            //  3       //   calculate which day based on given number
+                                    // ex ->  0 -> sunday
+                                        //    6-> saturaday
+                                        //    7-> sunday
+                                        //    8-> monday
+                                        //  50 -> monday
+                                        // 100->tuesday
+
+
+
+//                                            loops
+
+    //               what is loop?        .... ...  a loop is a sequence of instructions that is executed repeatedly in a code block, until a specified condition is met.
+                    //                         Any statement that ends when it meets a condition...
+
+                    // types of loops     
+                    //       while     do  while      for          for in         for of
+                     
+
+        // while loop
+        // 1.  while(condition){  //condition is true
+        //     //code to be executed
+        //     //code to be executed
+                        //   }
+
+// let n=60
+
+// while(n<10){
+//     console.log(n)
+//     console.log("You are in while loop")
+//            n--
+// }
+
+// display 10 to 20 using while loop      
+
+// let n =20
+
+// while(n<=20){
+//     console.log(n)
+//     n++
+// }
+
+
+// while(n>=10){
+//     console.log(n)
+//     n--
+// }
+// display table of 5  using while loop
+
+// let n=0;
+
+// while(n<50){
+   
+//      n=  n+5   
+//      console.log(n)
+// }
+
+// let n=1;
+// while(n<=10){
+//     console.log(n*5)
+//     n++
+// }
+
+// do while
+// let m= 40
+// do{
+//     console.log("Hello")
+//     m++
+// }while(m<5)
+
+// let p = 1;
+// do{
+//     console.log(p*5)
+//     p++
+// }while(p<11)
+
+// for loop
+
+// for(initialization ; condition ; operation ){
+
+    //code to be executed
+         //code to be executed
+
+// }
+
+
+
+// for(let i=10;i<=20;i++){    
+// console.log(i)
+// }
+
+// even no.s between  0-20 print
